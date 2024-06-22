94675f31b5142a930b309d377668d3cc862d62af
Danny Chen <155599374+dannywchen@users.noreply.github.com>
2024-06-22 04:38:26 -0400

    framework + firebase

---
+++ betterrussian/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLEq5RKOXsCPXCgjaxa3RAhbu5yu-N7_E",
+  authDomain: "betterrussian-25f5c.firebaseapp.com",
+  databaseURL: "https://betterrussian-25f5c-default-rtdb.firebaseio.com",
+  projectId: "betterrussian-25f5c",
+  storageBucket: "betterrussian-25f5c.appspot.com",
+  messagingSenderId: "287311448430",
+  appId: "1:287311448430:web:90f34912c07b2a3e2f597f",
+  measurementId: "G-E08585RDPG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
